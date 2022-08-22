f06c11fd10e0c95143e6495a51631d204d4bd188
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-22 08:33:48 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map(function(words){
+    const arrayOfSentences= words.split(" ")
+      const arrayOfCapitalizedSentences = arrayOfSentences.map(function(wording){
+        return wording[0].toUpperCase()+ wording.slice(1)
+      })
+        const titleCasedSentence = arrayOfCapitalizedSentences.join(" ")
+          return titleCasedSentence
+  })
+  }
+    titleCased();
